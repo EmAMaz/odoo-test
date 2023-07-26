50a5a719918430648b419202f4bb9fc68a930682
Santiago Maza <santiagomaza33@gmail.com>
2023-07-26 01:06:38 -0300

    Adding an odoo module

---
+++ estructure-basic/__manifest__.py
@@ -0,0 +1,26 @@
+{
+    'name': "Website Sale EXT Demo",
+    'summary': """
+        Modulo de prueba compatible para Odoo v15
+        Sales, Website
+    
+    """,
+    'description': """ 
+        Long description of module's purpose
+    
+    """,
+    #developer data's
+    'author': "sistemasdehudson",
+    'website': "http://www.google.com",
+    
+    'category': 'Sales',
+    'version': '0.1',
+    #any module necessary for this one to work correctly
+    'depends':['sale'],
+    #always loaded
+    'data': [],
+    # 'demo':[
+    #     'demo/demo.xml',
+    # ]
+    
+}
---
+++ estructure-basic/models/sale_order.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+    to_check = fields.Boolean(string="To check")
+    
